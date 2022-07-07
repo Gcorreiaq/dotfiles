571692035ea4086c3f379433df038296f6dff13e
Gabriel <estrangeiraconta@gmail.com>
2022-07-07 11:42:59 -0300

    changed node-focus

--- bspwm/node-focus-daemon.sh
+++ bspwm/node-focus-daemon.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+above() {
+    for w in $1;do
+        bspc node "$w" -l above
+    done
+}
+
+below() {
+    for w in $1;do
+        bspc node "$w" -l below
+    done
+}
+normal() {
+    for w in $1;do
+        bspc node "$w" -l normal
+    done
+}
+
 while bspc subscribe -c 1 node_focus; do
-    if [[ -n $(bspc query -N -n "$(bspc query -N -n last).sticky") ]];then
-        bspc node last -l above
+    floating=$(bspc query -N -n '.local.window.!hidden.!sticky.floating')
+    tiled=$(bspc query -N -n '.local.window.!hidden.!sticky.!floating')
+
+    if [[ $(bspc query -N -n 'focused.!floating') ]];then
+        above "$tiled"
+        normal "$floating"
     else
-        bspc node last -l normal
-    fi
-    bspc node focused -l above
+        below "$floating"
+        bspc node -l above
+        normal "$tiled"
+    fi 
 done
